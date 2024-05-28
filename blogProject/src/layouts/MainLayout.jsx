c8361e0f21d14ab109aa118b7fd022849375fbd9
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-05-28 19:31:42 +0330

    Add MainLayout and Navbar

---
+++ blogProject/src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import {Outlet} from "react-router-dom";
+import Navbar from "../components/Navbar.jsx";
+
+const MainLayout = () => {
+    return(
+        <>
+            <Navbar />
+            <div>
+                <Outlet />
+            </div>
+        </>
+
+    )
+}
+export default MainLayout;
--- blogProject/src/main.jsx
+++ blogProject/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import { RouterProvider} from "react-router-dom";
+import  {router} from "./routes/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>
 )
