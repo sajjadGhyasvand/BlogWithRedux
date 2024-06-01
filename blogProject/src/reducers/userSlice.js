4b8e112b81c491799317aaa0febb3708121d1940
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-06-01 19:39:25 +0330

    CRUD On Authors

--- blogProject/src/reducers/userSlice.js
+++ blogProject/src/reducers/userSlice.js
@@ -1,21 +1,44 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { getAllUsers } from "../services/blogsServices";
+import { createUser, deleteUser, getAllUsers } from "../services/blogsServices";
 
 export const fetchUsers = createAsyncThunk("/users/fetchUsers", async () => {
     const response = await getAllUsers();
     return response.data;
 });
 
+export const deleteApiUser = createAsyncThunk(
+    "/users/deleteApiUser",
+    async (initialUserId) => {
+        await deleteUser(initialUserId);
+        return initialUserId;
+    }
+);
+
+export const addNewUser = createAsyncThunk(
+    "/users/addNewUser",
+    async (initialUser) => {
+        const response = await createUser(initialUser);
+        return response.data;
+    }
+);
+
 const usersSlice = createSlice({
     name: "users",
     initialState: [],
     reducers: {},
-    extraReducers(builder) {
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload;
-            //With returing a new result Immer will replace existing state with
-            //whatever we return
-        });
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                return action.payload;
+                //With returing a new result Immer will replace existing state with
+                //whatever we return
+            })
+            .addCase(addNewUser.fulfilled, (state, action) => {
+                state.push(action.payload);
+            })
+            .addCase(deleteApiUser.fulfilled, (state, action) => {
+                return state.filter((user) => user.id !== action.payload);
+            });
     },
 });
 
